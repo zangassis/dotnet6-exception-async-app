e2c1455d78fdd10364fda9a3b5a054f23dd1a1e0
Assis Zang <assis.compasso@dotzmkt.com.br>
2021-10-19 09:30:39 -0300

    feat: Project creation

---
+++ ConceptApp/Program.cs
@@ -0,0 +1,19 @@
+﻿await ValidateException();
+
+static async Task ValidateException() =>
+    await PersonalException();
+
+static async Task PersonalException()
+{
+    Task task = Task.Run(() => throw new ArgumentException("Exception Task 1"));
+    Task secondTask = Task.Run(() => throw new NullReferenceException("Exception Task 2"));
+
+    try
+    {
+      Task.WaitAll(task, secondTask);
+    }
+    catch (AggregateException ex)
+    {
+        ex.InnerExceptions.ToList().ForEach(x => Console.WriteLine(x.Message));
+    }
+}
